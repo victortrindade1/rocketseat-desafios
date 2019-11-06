692f8626e5c8620579fdbe6dc5f8d24bd64f80cc
Victor <victortrindade@gmail.com>
2019-11-06 20:30:54 -0300

    validaçao com yup

--- desafio02/src/routes.js
+++ desafio02/src/routes.js
@@ -3,14 +3,13 @@ import { Router } from 'express';
 import authMiddleware from './app/middlewares/auth';
 
 import SessionController from './app/controllers/SessionController';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
 routes.post('/sessions', SessionController.store);
 
-// Verifica se já é usuário logado
-routes.use(authMiddleware);
-
-// aqui mais rotas
+// authMiddleware -> Verifica se é user admin
+routes.post('/users', authMiddleware, UserController.store);
 
 export default routes;
